455808a920c7b852427fc642e0390d1a4c5ddb56
Pedro Peixoto <126399622+Peixotim@users.noreply.github.com>
2024-06-20 13:37:08 -0300

    Add files via upload
    Login do site feito

--- login.php
+++ login.php
@@ -1,32 +1,64 @@
-<?php 
-$hostname=  "localhost";
+<?php
+$hostname =  "localhost";
 $username = "root";
 $password = "";
-$database = "Ieat";
 
-$mysqli = new mysqli($hostname,$username,$password,$database);
-if($mysqli -> connect_error){
-    echo "Falha na conexão" . $mysqli -> connect_error;
+$mysqli = new mysqli($hostname, $username, $password,);
+if ($mysqli->connect_error) {
+    echo "Falha na conexão" . $mysqli->connect_error;
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="/foto.ico">
+    <title>Pagina de login i eat</title>
 </head>
+
 <body>
-    <div class="Inputs">
-        Nome: <input type="text" placeholder="Nome"><br></br>
-        Email: <input type="email" placeholder="Email"><br></br>
-        Senha: <input type="password" placeholder="Senha"><br></br>
-        Telefone: <input type="number" placeholder="Telefone"><br></br>
+
+    <div class="container" id="container">
+        <div class="form-container Registrar">
+            <form>
+                <h1>Crie Sua Conta!</h1>
+                <input type="text" placeholder="Nome" required>
+                <input type="email" placeholder="Email" required>
+                <input type="password" placeholder="Senha" required>
+                <button>Registrar</button>
+            </form>
+        </div>
+        <div class="form-container login">
+            <form>
+                <h1>Faça seu login!</h1>
+                <input type="email" placeholder="Email" required>
+                <input type="password" placeholder="Senha" required>
+                <a href="#">Esqueceu sua senha?</a>
+                <button>Login</button>
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Bem vindo de volta!</h1>
+                    <p>Caso já possua uma conta faça seu login com ela!</p>
+                    <button class="hidden" id="login">Login</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Olá caro comedor!</h1>
+                    <p>Registre sua conta para utilizar o site e pedir altas gostosuras!</p>
+                    <button class="hidden" id="register">Registrar-se</button>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="btn">
-        <button>Registre</button>
-    </div>
+
+    <script src="script.js"></script>
 </body>
+
 </html>
